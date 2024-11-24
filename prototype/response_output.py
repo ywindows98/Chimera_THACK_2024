499cf666101fc8eb98d7352de73d004cbc3750b0
Dmytro Varich <varich.it@gmail.com>
2024-11-24 09:59:15 +0100

    update database

--- prototype/response_output.py
+++ prototype/response_output.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-data_frame_path = "default/default_data.csv"
+# Define path to your CSV file
+data_frame_path = "user_dataset/current_data.csv"
+
+# Load the data into a DataFrame
 df = pd.read_csv(data_frame_path)
 
-# Calculate mean math and writing scores by parental education level
-mean_scores = df.groupby('parental_level_of_education')[['math_score', 'writing_score']].mean()
+# Group the data by 'Make' and sum the CO2 emissions
+emissions_by_make = df.groupby('Make')['CO2 Emissions(g/km)'].sum().sort_values()
 
-# Plot bar graph
-ax = mean_scores.plot(kind='bar', figsize=(10, 6))
-plt.title('Average Math and Writing Scores by Parental Level of Education')
-plt.xlabel('Parental Level of Education')
-plt.ylabel('Average Score')
-plt.xticks(rotation=45)
+# Plot the bar graph
+plt.figure(figsize=(12, 8))
+emissions_by_make.plot(kind='bar')
+plt.title('CO2 Emissions by Auto Manufacturer')
+plt.xlabel('Manufacturer')
+plt.ylabel('Total CO2 Emissions (g/km)')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
 
-# Save figure
-plt.savefig('./figures/average_scores_by_parental_education.png', transparent=True)
-plt.show()
+# Save plot to a file
+plt.savefig('./figures/co2_emissions_by_manufacturer.png', transparent=True)
+plt.close()
